9fcc65cabe047f163fe15e7ef655ee9bf31cfaf9
DeadWinnerv <vitwinner@ya.ru>
2023-05-06 00:07:16 +0300

    base game fncs added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { WORDS } from 'src/constants/WORDS';
+
+type answer = {
+  char: string,
+  present?: boolean
+  onPosition?: boolean
+} | undefined
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WordleGame';
+  word: string;
+  currentWord: string = '';
+  answers: answer[][] = [[],[],[],[],[]]
+  currentRow: number = 0;
+  rows: number[] = [0,1,2,3,4]
+  columns: number[] = [0,1,2,3,4]
+  inputGroup = new FormGroup ({
+    char: new FormControl('')
+  })
+
+  ngOnInit(): void {
+    this.generateWord()
+    console.log(this.word);
+  }
+
+  generateWord() {
+    this.word = WORDS.at(Math.floor(Math.random() * WORDS.length))!
+    console.log(Math.floor(Math.random() * WORDS.length));
+  }
+
+  checkWord() {
+    if (this.currentWord !== this.word) {
+      for (let i = 0; i < 5; i++) {
+        console.log(this.word[i] + ' ' + this.currentWord[i]);
+        
+        if (this.word[i] === this.currentWord[i]) {
+          this.answers[this.currentRow][i] = {
+            char: this.currentWord[i],
+            onPosition: true
+          }
+        } else if (this.word.includes(this.currentWord[i])) {
+          
+          this.answers[this.currentRow][i] = {
+            char: this.currentWord[i],
+            present: true
+          }
+        } else {
+          this.answers[this.currentRow][i] = {
+            char: this.currentWord[i]
+          }
+        }
+      }
+    } else {
+      
+    }
+    console.log(this.answers);
+    
+    this.currentRow++
+    this.currentWord = ''
+  }
+
+  handleInputChange() {
+    this.currentWord = this.currentWord + this.inputGroup.value.char?.toLocaleLowerCase()
+    if (this.currentWord.length < 5) {
+      console.log(this.currentWord)
+    } else {
+      this.checkWord()
+    }
+    
+  }
 }
